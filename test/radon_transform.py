4983836276178dc302e343b041850a8c7661a312
chulhoon <chulhoonjang@gmail.com>
2017-12-07 15:52:53 +0900

    template 생성 및 marginal space 생성 및 image cropping 코드 추가 pair 생성 후 분류하는 알고리즘 추가

--- test/radon_transform.py
+++ test/radon_transform.py
@@ -7,6 +7,7 @@ Created on Wed Dec  6 15:13:38 2017
 import peakutils
 import numpy as np
 import matplotlib.pyplot as plt
+import cv2
 
 def findCrossPoint(L1, L2):
     x = (L1[1] - L2[1]) / (L2[0] - L1[0])
@@ -19,7 +20,7 @@ def generateRadonLines(radon_prj, theta, ang_idx, thresh, dist, center, img_size
     
     radon_lines = []
     for idx in indexes:
-        l = radon_line()
+        l = ps_line([(20, 30), (48, 60)])
         l.generate(idx, theta[ang_idx], center, img_size)
         radon_lines.append(l)
     
@@ -30,11 +31,99 @@ def generateRadonLines(radon_prj, theta, ang_idx, thresh, dist, center, img_size
         plt.show()
     
     return radon_lines
+
+def createTemplate(src, dist, margin):
+    img_lower = np.zeros(src.shape, np.uint8)
+    img_lower = cv2.line(img_lower, (0, margin), (margin*2+np.int(dist), margin), (255,255,255), 2)
+    img_lower = cv2.line(img_lower, (margin, margin), (margin, margin*2), (255,255,255), 2)
+    img_lower = cv2.line(img_lower, (margin+np.int(dist), margin), (margin+np.int(dist), margin*2), (255,255,255), 2)
+
+    img_upper = np.zeros(src.shape, np.uint8)
+    img_upper = cv2.line(img_upper, (0, margin), (margin*2+np.int(dist), margin), (255,255,255), 2)
+    img_upper = cv2.line(img_upper, (margin, margin), (margin, 0), (255,255,255), 2)
+    img_upper = cv2.line(img_upper, (margin+np.int(dist), margin), (margin+np.int(dist), 0), (255,255,255), 2)
+
+    img_template = [img_lower, img_upper]    
+    
+    return img_template
+
+
+def drawRectangle(src, pts):
+    dst = src
+    
+    dst = cv2.line(dst, 
+                  (np.int(pts[0][0]), np.int(pts[0][1])),
+                  (np.int(pts[1][0]), np.int(pts[1][1])),
+                  (255,0,0), 1)
+    dst = cv2.line(dst, 
+                  (np.int(pts[1][0]), np.int(pts[1][1])),
+                  (np.int(pts[2][0]), np.int(pts[2][1])),
+                  (255,0,0), 1)
+    dst = cv2.line(dst, 
+                  (np.int(pts[2][0]), np.int(pts[2][1])),
+                  (np.int(pts[3][0]), np.int(pts[3][1])),
+                  (255,0,0), 1)
+    dst = cv2.line(dst, 
+                  (np.int(pts[3][0]), np.int(pts[3][1])),
+                  (np.int(pts[0][0]), np.int(pts[0][1])),
+                  (255,0,0), 1)
+    return dst
+
+def calculateMarginalSpace(pt1, pt2, margin):            
+    ang = 90 / 180 * np.pi # degree > radian
+    
+    dx = pt2[0] - pt1[0]
+    dy = pt2[1] - pt1[1]
+       
+    dist = np.sqrt(dx*dx + dy * dy)
+       
+    # unit vector
+    ux = dx/dist
+    uy = dy/dist
+    
+    # rotation
+    ux_rot = np.cos(ang) * ux - np.sin(ang) * uy
+    uy_rot = np.sin(ang) * ux + np.cos(ang) * uy
+    
+    pt1 = (pt1[0] - margin * ux, pt1[1] - margin * uy)
+    pt2 = (pt2[0] + margin * ux, pt2[1] + margin * uy)
+    
+    point1 = (pt1[0] - margin * ux_rot, pt1[1] - margin * uy_rot)
+    point2 = (pt2[0] - margin * ux_rot, pt2[1] - margin * uy_rot)
+    point3 = (pt2[0] + margin * ux_rot, pt2[1] + margin * uy_rot)
+    point4 = (pt1[0] + margin * ux_rot, pt1[1] + margin * uy_rot)
+    
+    space = [point1, point2, point3, point4]    
+    
+    return space, dist
+    
+def cropImage(space, img, img_crop_size):    
+    arr_space = np.array(space)
+        
+    point1 = (arr_space[0,0], arr_space[0,1])
+    point2 = (arr_space[1,0], arr_space[1,1])
+    point3 = (arr_space[2,0], arr_space[2,1])
+    point4 = (arr_space[3,0], arr_space[3,1])
+    
+    CropXLen = np.sqrt((point2[0]-point1[0])**2 + (point2[1]-point1[1])**2)
+    CropYLen = np.sqrt((point3[0]-point2[0])**2 + (point3[1]-point2[1])**2)
+    
+    pt_origin = [point1[0], point1[1], point2[0], point2[1], point3[0], point3[1], point4[0], point4[1]]
+    pt_origin = np.array(pt_origin, dtype=np.float32).reshape((4,2))
+    
+    pt_transform = [0, 0, CropXLen, 0, CropXLen, CropYLen, 0, CropYLen]
+    pt_transform = np.array(pt_transform, dtype=np.float32).reshape((4,2))
+    
+    Trans_mat = cv2.getPerspectiveTransform(pt_origin,pt_transform)
+    img_crop = cv2.warpPerspective(img, Trans_mat, img_crop_size)
+    
+    return img_crop
     
 class radon_line:
     def __init__(self):
         self.ang = 0.
         self.pos = [0.0, 0.0]
+        self.pts = []
         
     def generate(self, offset, ang_ps1, center, img_size):
         # angle
@@ -74,3 +163,87 @@ class radon_line:
             return self.b
         else:
             return (- self.b / self.a)
+    
+        
+class ps_line(radon_line):
+    def __init__(self, length_limit):
+        radon_line.__init__(self)
+        self.pts = []
+        self.valid_pts = []
+        self.invalid_pts = []
+        self.per_pairs = [] # perpendicular
+        self.par_pairs = [] # parallel
+        self.length_limit = {'perpendicular': length_limit[0], 'parallel': length_limit[1]}
+        self.per_spaces = []
+        
+        
+    def set_cross_pt(self, pt):
+        self.pts.append(pt)
+    
+    def classify_valid_cross_pt(self, img):
+        for pt in self.pts:
+            if img[np.int(pt[1]), np.int(pt[0])] == 0:
+                self.invalid_pts.append(pt)
+            else:
+                self.valid_pts.append(pt)
+        self.valid_pts = sorted(self.valid_pts, key=lambda tup: tup[0])               
+        self.invalid_pts = sorted(self.invalid_pts, key=lambda tup: tup[0])
+    
+    def generate_pairs(self):
+        if len(self.pts) >= 2:
+            for i in range(len(self.pts)-1):
+                for j in range(i,len(self.pts)):
+                    p1 = self.pts[i]
+                    p2 = self.pts[j]
+                    dist = np.sqrt((p1[0]-p2[0])*(p1[0]-p2[0]) + (p1[1]-p2[1])*(p1[1]-p2[1]))
+                    
+                    if dist >= self.length_limit['perpendicular'][0] and dist <= self.length_limit['perpendicular'][1]:
+                        pairs = (p1, p2)
+                        self.per_pairs.append(pairs)
+                        #print('perpendicular: {:2.2f} pixels'.format(dist))
+                    elif dist >= self.length_limit['parallel'][0] and dist <= self.length_limit['parallel'][1]:
+                        pairs = (p1, p2)
+                        self.par_pairs.append(pairs)
+                       # print('parallel: {:2.2f} pixels'.format(dist))
+                       
+    def classify_pairs(self, img, margin, debug = False):
+        for pair in self.per_pairs:
+            marginal_space, dist = calculateMarginalSpace(pair[0], pair[1], margin)
+            img_crop = cropImage(marginal_space, img, (np.int(dist+margin*2), margin*2))
+            slot_temp = createTemplate(img_crop, dist, margin)
+            slot_temp = createTemplate(img_crop, dist, margin)            
+            
+            results = []
+            for template in slot_temp:
+                result = cv2.matchTemplate(img_crop, template, cv2.TM_CCORR_NORMED)
+                results.append(result)
+            
+            
+            
+            if debug == True:
+                plt.imshow(img_crop, cmap = 'gray')                
+                plt.show()
+                for template, result in zip(slot_temp, results):    
+                    plt.imshow(template, cmap = 'gray')    
+                    plt.show()              
+                    print(result)
+            
+        for pair in self.par_pairs:
+            marginal_space, dist = calculateMarginalSpace(pair[0], pair[1], margin)
+            img_crop = cropImage(marginal_space, img, (np.int(dist+margin*2), margin*2))
+            slot_temp = createTemplate(img_crop, dist, margin)
+            slot_temp = createTemplate(img_crop, dist, margin)            
+            
+            results = []
+            for template in slot_temp:
+                result = cv2.matchTemplate(img_crop, template, cv2.TM_CCORR_NORMED)
+                results.append(result)
+            
+            if debug == True:
+                plt.imshow(img_crop, cmap = 'gray')                
+                plt.show()
+                for template, result in zip(slot_temp, results):    
+                    plt.imshow(template, cmap = 'gray')    
+                    plt.show()              
+                    print(result)
+        
--- test/test_RadonTransform.py
+++ test/test_RadonTransform.py
@@ -6,28 +6,45 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 from skimage.io import imread
 from skimage.transform import radon
-from radon_transform import generateRadonLines, findCrossPoint
+from radon_transform import generateRadonLines, findCrossPoint, calculateMarginalSpace, drawRectangle, cropImage, createTemplate
 
 import time
+
 #root = 'C:/Users/chulh/문서/Git/avm_dataset/test/'
-set_num = 8
+set_num = 2
 root = 'C:/Users/chulh/문서/Git/avm_dataset/dataset/hyu_171121/ss/set{}/labeled/class_1'.format(set_num)
 output_dir = 'C:/Users/chulh/Documents/hyu_171121/set{}/'.format(set_num)
 if os.path.isdir(output_dir) == False:
     os.mkdir(output_dir)
 
-f0 = 0
-f1 = 541
+f0 = 156
+f1 = 157
 
-ang_init = 90
+ang_init = 120
 ang_margin = 10
 ang_center = ang_init 
+img_rescale = 0.25
 
 for i in tqdm(range(f0,f1)):        
     img_file = os.path.join(root, '{:08d}.jpg'.format(i))
-    img = imread(img_file, as_grey=True)
-    img = cv2.resize(img, None, fx=0.25, fy=0.25)
+    img = imread(img_file, as_grey=True)    
+    img_org = img
+
+    kernel = np.ones((5,5),np.uint8)
+    img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)    
+#    plt.imshow(img)
+#    plt.show()
     
+    img = cv2.erode(img,kernel,iterations = 1)
+#    plt.imshow(img)
+#    plt.show()
+    
+    img = cv2.resize(img, None, fx=img_rescale, fy=img_rescale)
+    img[img>0] = 255
+#    plt.imshow(img)
+#    plt.show()
+
+    img_size = (img.shape[1], img.shape[0])
     start_time = time.time()
     # rdaon transform
     theta = []    
@@ -45,7 +62,7 @@ for i in tqdm(range(f0,f1)):
     ang_ps1 = theta[ang_idx]
         
     # line generation for principle angle 1
-    lines_ps1 = generateRadonLines(prj_t, theta, ang_idx, 0.2, 10, center, (img.shape[1], img.shape[0]))
+    lines_ps1 = generateRadonLines(prj_t, theta, ang_idx, 0.2, 10, center, img_size)
     
     # line generation for principle angle 2
     ang_ps2 = 0
@@ -59,37 +76,58 @@ for i in tqdm(range(f0,f1)):
     theta = [ang_ps2]
     prj_t = radon(img, theta=theta)    
     lines_ps2 = generateRadonLines(prj_t, theta, 0, 0.2, 10, center, (img.shape[1], img.shape[0]))
+        
+    img_debug = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
     
-    # cross point
-    pts = []
+    pts = []    
+    margin = 5
     for l1 in lines_ps1:    
         for l2 in lines_ps2:
+            # cross point
             x, y = findCrossPoint((l1.get_a(), l1.get_b()), (l2.get_a(), l2.get_b()))
-            pts.append((x,y))
-    print('execution time: {} ms'.format((time.time()-start_time)*1000))
+            if x >= 0 and x < img_size[0] and y >= 0 and y < img_size[1]:
+                l1.set_cross_pt((x,y))
+        l1.generate_pairs()        
+        l1.classify_pairs(img, margin, True)                
+        #img_debug = drawRectangle(img_debug, marginal_space)                        
+    #print('execution time: {} ms'.format((time.time()-start_time)*1000))
        
     # ang center update
     ang_center = ang_ps1
-    print(ang_center)
+    #print(ang_center)
 
     # draw
     img_debug = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
     
-    for l in lines_ps1:
-        img_debug = cv2.line(img_debug, (np.int(l.end_pts[0][0]), np.int(l.end_pts[0][1])),
-                            (np.int(l.end_pts[1][0]), np.int(l.end_pts[1][1])), (255,0,0), 1)
+#    for l in lines_ps2:
+#        img_debug = cv2.line(img_debug, (np.int(l.end_pts[0][0]), np.int(l.end_pts[0][1])),
+#                            (np.int(l.end_pts[1][0]), np.int(l.end_pts[1][1])), (255,0,0), 1)
         
-    for l in lines_ps2:
-        img_debug = cv2.line(img_debug, (np.int(l.end_pts[0][0]), np.int(l.end_pts[0][1])),
-                            (np.int(l.end_pts[1][0]), np.int(l.end_pts[1][1])), (0,0,255), 1)
-    for pt in pts:
-        img_debug = cv2.circle(img_debug, (np.int(pt[0]), np.int(pt[1])), 1, (255,255,0), 1)
-    
-    save_file = os.path.join(output_dir, '{:08d}.png'.format(i))
-    
-    plt.imshow(img_debug)
+    for l in lines_ps1:    
+#        img_debug = cv2.line(img_debug, (np.int(l.end_pts[0][0]), np.int(l.end_pts[0][1])),
+#                            (np.int(l.end_pts[1][0]), np.int(l.end_pts[1][1])), (0,0,255), 1)        
+#        for pt in l.pts:
+#            img_debug = cv2.circle(img_debug, (np.int(pt[0]), np.int(pt[1])), 1, (0,255,255), -1)
+            
+        for pair in l.per_pairs:
+            #img_debug = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+            img_debug = cv2.circle(img_debug, (np.int(pair[0][0]), np.int(pair[0][1])), 1, (255,0,255), -1)
+            img_debug = cv2.circle(img_debug, (np.int(pair[1][0]), np.int(pair[1][1])), 1, (255,0,255), -1)
+            #print(pair)
+            #plt.imshow(img_debug)
+            #plt.show()
+            
+        for pair in l.par_pairs:
+            #img_debug = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+            img_debug = cv2.circle(img_debug, (np.int(pair[0][0]), np.int(pair[0][1])), 1, (255,0,255), -1)
+            img_debug = cv2.circle(img_debug, (np.int(pair[1][0]), np.int(pair[1][1])), 1, (255,0,255), -1)
+
+
+    plt.imshow(img_debug)    
     plt.show()
-    #cv2.imwrite(save_file, img_debug)
+
+#    save_file = os.path.join(output_dir, '{:08d}.png'.format(i))
+#    cv2.imwrite(save_file, img_org_debug)
         
 
 
